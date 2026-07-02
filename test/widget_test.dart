6848f699b67ff457f69a4a1f4caff5f1362f7d13
aarushlohit <141929019+aarushlohit@users.noreply.github.com>
2026-07-02 16:46:39 +0530

    test: init Hive and open boxes before pumping app in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,10 +7,25 @@
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:gentleman/main.dart';
+import 'package:gentleman/core/constants/app_constants.dart';
+import 'package:gentleman/core/models/protected_app.dart';
+import 'package:gentleman/core/models/protection_event.dart';
 
 void main() {
   testWidgets('App renders smoke test', (WidgetTester tester) async {
+    // Initialize Hive as the app expects boxes to be available synchronously.
+    await Hive.initFlutter();
+    Hive.registerAdapter(AppProtectionStatusAdapter());
+    Hive.registerAdapter(ProtectedAppAdapter());
+    Hive.registerAdapter(InteractionTypeAdapter());
+    Hive.registerAdapter(ProtectionResultAdapter());
+    Hive.registerAdapter(ProtectionEventAdapter());
+    await Hive.openBox<dynamic>(AppConstants.settingsBox);
+    await Hive.openBox<ProtectionEvent>(AppConstants.statisticsBox);
+    await Hive.openBox<ProtectedApp>(AppConstants.rulesBox);
+
     await tester.pumpWidget(const ProviderScope(child: GentlemanApp()));
     expect(find.text('Gentleman'), findsWidgets);
   });
